b320b1ba572607bcc74580cbb25d75a4657702e8
Renilson Andrade <renilsonandrade@hotmail.com>
2019-11-12 13:27:59 -0300

    Add project files.

---
+++ TesteQuartz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteQuartz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
